371f91bf3b96d089bc3d2d2cf05da00f59ddbf33
childes <1489613541@qq.com>
2023-04-19 14:05:17 +0800

    获取数据方法

---
+++ by/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">Hello</text>
+		</view>
+		<button @click="golist()">golist</button>
+	</view>
+</template>
+
+<script setup>
+	let golist=()=>{
+		// uni.navigateTo({
+		// 	url:"/pages/list/list?x=10"
+		// })
+	}
+	uni.request({
+		url:"/api/home/floorlist",
+		// url:"/api/rmsp",
+		method:'GET',
+		header: {
+			// "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+			// 'Access-Control-Allow-Origin': '*'
+		},
+		success: (res) => {
+		    console.log(res);
+		},
+		// fail:(err)=>{
+		// 	console.log("错误",err);
+		// }
+	})
+</script>
+
+<style>
+	
+</style>
---
+++ by/pages/list/list.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		接收index的参数:{{x}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				x:"1"
+			};
+		},
+		onLoad(option){
+			console.log(this.$page.options.x);
+			this.$data.x=this.$page.options.x
+		}
+		
+	}
+</script>
+
+<style lang="scss">
+
+</style>
