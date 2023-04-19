7994ce23ff41e5b105910c4b7d5681c9508844e2
childes <1489613541@qq.com>
2023-04-19 10:10:22 +0800

    创建内容 04-19-10

---
+++ by/pages/list/list.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		list页面
+	</view>
+</template>
+
+<script>
+	export default {
+		// data() {
+		// 	return {
+				
+		// 	};
+		// }
+		onLoad(option){
+			console.log(this.$page.options)
+		}
+	}
+
+</script>
+
+<style lang="scss">
+
+</style>
